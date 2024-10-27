024f635f94cac2d08395312decbf3a07792f2034
Aloterra <ripallol.541@yandex.ru>
2024-10-27 15:19:32 +0300

    Редактирование связи между проектами и добавление Extentions

---
+++ Forecast.Infarastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Forecast.Infrastructure.Data;
+
+namespace Forecast.Infrastructure.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddForecastDbContext(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<ForecastDbContext>(options =>
+                options.UseSqlite(connectionString));
+            return services;
+        }
+    }
+}
